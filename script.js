204e0411b5ef4b85f65e5b89846c29ed1c773b9b
UnesseAh <youness.ahasla@gmail.com>
2022-12-26 16:44:50 +0100

    display the score of the user

--- script.js
+++ script.js
@@ -1,18 +1,153 @@
-const startQuizButton = document.getElementById("startQuiz");
-const secondStep = document.getElementById("secondStep");
-const thirstStep = document.getElementById("thirdStep");
+/************************** Variable Declarations ***********************/
+//Stepper Elements :
+const stepperSecondIcon = document.querySelector("#secondStep");
+const stepperThirdIcon = document.querySelector("#thirdStep");
+//Components :
+const startQuizButton = document.querySelector("#startQuiz");
+const getResults = document.querySelector("#getResults");
+const nextQuestion = document.querySelector("#nextQuestion");
+let progressWidth = document.querySelector(".progressCounter");
+//Quiz Sections :
+const informationSection = document.querySelector("#information-section");
+const quizSection = document.querySelector("#quiz-section");
+const resultsSection = document.querySelector("#results-section");
+//Question & Options :
+let question = document.querySelector(".question");
+let answerA = document.querySelector(".answerA");
+let answerB = document.querySelector(".answerB");
+let answerC = document.querySelector(".answerC");
+let answerD = document.querySelector(".answerD");
 
-let getResults = document.getElementById("getResults");
-getResults.addEventListener("click", () => thirstStep.classList.add("completed"));
 
+/********************* Hide Quiz Section & Result Section *******************/
 
-startQuizButton.addEventListener("click",()=>{
-  secondStep.classList.add("completed")
-
-  fetchData();
-
-
+document.addEventListener("DOMContentLoaded", function() {
+  quizSection.style.display = "none";
+  resultsSection.style.display = "none";
 });
+
+/*************************** Start Quiz ***************************/
+
+startQuizButton.addEventListener("click", () => {
+  stepperSecondIcon.classList.add("completed");
+  quizSection.style.display = "";
+  informationSection.style.display = "none";
+  progressWidth.style.width = "10%";
+  displayQuestions();
+});
+
+/********************** Shuffle Array ***********************/
+
+const shuffledQuestions = questions.sort(() => Math.random() - .5);
+
+/******************** Display Questions ********************/
+
+let arrayIndex = 0;
+
+function displayQuestions(){
+  question.textContent = shuffledQuestions[arrayIndex].question;
+  answerA.textContent = shuffledQuestions[arrayIndex].choiceA;
+  answerB.textContent = shuffledQuestions[arrayIndex].choiceB;
+  answerC.textContent = shuffledQuestions[arrayIndex].choiceC;
+  answerD.textContent = shuffledQuestions[arrayIndex].choiceD;
+  quizCountdown();
+}
+
+/******************** Quiz Countdown *******************/
+
+const counterElement = document.querySelector("#timeCounter");
+let counterTime;
+
+function quizCountdown(){
+  counterTime = setInterval(() => {
+    if(counterElement.textContent != 0){
+      counterElement.textContent -= 1;
+    }else{
+      clearInterval(counterTime);
+      counterElement.textContent = 10;
+      newArray.push("");
+      nextQuestion.click();
+    }
+  }, 1000);
+}
+
+/********************* Progress Bar Counter *******************/
+
+function progressBar(){
+  progressBarValue += 10;
+  if(progressBarValue <= 100){
+    clearInterval(counterTime);
+    counterElement.textContent = 10;
+    progressWidth.style.width = `${progressBarValue}%`;
+    arrayIndex++;
+    displayQuestions();
+  }
+}
+
+/********************* Redirect User to Next Question *******************/
+
+let progressBarValue = 10;
+
+nextQuestion.addEventListener("click", () => {
+  if(arrayIndex < 9){
+      progressBar();
+  }else{
+    stepperThirdIcon.classList.add("completed")
+    quizSection.style.display = "none";
+    resultsSection.style.display = "block"
+  }
+})
+
+/********************* Count Score *******************/
+
+let card = document.querySelectorAll(".card");
+let newArray = [];
+let correctAnswers = 0;
+
+card.forEach((cards) => {
+  cards.onclick = () => {
+    if(cards.children[0].textContent === questions[arrayIndex].correct){
+      correctAnswers++;
+      userScore.textContent += `${correctAnswers}/10`;
+    }
+    newArray.push(cards.children[0].textContent);
+    
+    nextQuestion.click();
+    }
+})
+
+/********************* Display Results *******************/
+const showResultsButton = document.querySelector('#showResults');
+const resultsContainer = document.querySelector('.resultsContainer');
+const userScore = document.querySelector('.userScore');
+
+showResultsButton.addEventListener("click", () => {
+  console.log(1);
+  resultsContainer.classList.remove('hide');
+  displayResults();
+});
+
+// function displayResults(){
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // $(function () {
@@ -23,133 +158,3 @@ startQuizButton.addEventListener("click",()=>{
 //         $("#information-section").hide();
 //     })
 // })
-
-document.addEventListener("DOMContentLoaded", function() {
-    const quizSection = document.querySelector("#quiz-section");
-    const resultsSection = document.querySelector("#results-section");
-    const startQuizButton = document.querySelector("#startQuiz");
-    const informationSection = document.querySelector("#information-section");
-  
-    quizSection.style.display = "none";
-    resultsSection.style.display = "none";
-
-    startQuizButton.addEventListener("click", function() {
-      quizSection.style.display = "";
-      informationSection.style.display = "none";
-      progressCounter.style.width = "10%";
-
-
-    });
-  });
-var counterTime;
-let question = document.querySelector(".question")
-let answereA = document.querySelector(".answereA")
-let answereB = document.querySelector(".answereB")
-let answereC = document.querySelector(".answereC")
-let answereD = document.querySelector(".answereD")
-
-
-let card=document.querySelectorAll(".card");
-let newArray = [];
-let countCorrect = 0;
-
-card.forEach((cards)=>{
-    cards.onclick=()=>{
-      // console.log(cards.children[0].textContent);
-      // console.log(questions[counter].correct)
-      if(cards.children[0].textContent===questions[counter].correct){
-        countCorrect++;
-      }
-      newArray.push(cards.children[0].textContent);
-      x += 10;
-      if(x<=100){
-        clearInterval(counterTime);
-        countermElement.innerHTML=10;
-        progressCounter.style.width = `${x}%`;
-        counter++;
-        fetchData();
-      }
-    }
-})
-// console.log(countCorrect)
-
-
-// function checkCorrectAnswer(){
-//   let i = 0;
-//   while(i < newArray.length){
-//     console.log(i);
-//     console.log(countCorrect);
-//     if(newArray[i] === questions[i].correct){
-//       countCorrect++;
-
-//     }
-//     i++
-//   }
-//   return countCorrect;
-// }
-
-
-
-
-const shuffledQuestions = questions.sort(() => Math.random() - .5);
-
-console.log(shuffledQuestions);
-let counter = 0;
-
-function fetchData(){
- 
-    question.innerHTML = shuffledQuestions[counter].question;
-    answereA.innerHTML = shuffledQuestions[counter].choiceA;
-    answereB.innerHTML = shuffledQuestions[counter].choiceB;
-    answereC.innerHTML = shuffledQuestions[counter].choiceC;
-    answereD.innerHTML = shuffledQuestions[counter].choiceD;
-    quizCountdown();
-  
-}
-
-
-let nextQuestion = document.getElementById("nextQuestion");
-
-let progressCounter = document.querySelector(".progressCounter");
-
-let x = 10;
-
-nextQuestion.addEventListener("click", () => {
-  if(counter<10){
-    x += 10;
-  
-    if(x<=100){
-      clearInterval(counterTime);
-      countermElement.innerHTML=10;
-      progressCounter.style.width = x  + "%";
-      console.log(x);
-      counter++;
-      fetchData();
-    }
-  }else{
-    quizSection.style.display="none"
-  }
-  
-  
-
-})
-
-var countermElement = document.getElementById("timeCounter");
-
-
-function quizCountdown(){
-  // console.log(countermElement)
-   counterTime = setInterval(()=>{
-    if(countermElement.innerHTML!=0){
-      countermElement.innerHTML -= 1;
-    }else{
-      clearInterval(counterTime);
-      countermElement.innerHTML = 10;
-      newArray.push("");
-      nextQuestion.click();
-    }
-  }, 1000);
-}
-
-console.log(newArray);
-
